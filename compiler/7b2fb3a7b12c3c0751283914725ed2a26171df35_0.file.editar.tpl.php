e6f91b2b258891ed2efad6e41642035f6805d472
loro102 <loro102@gmail.com>
2016-05-10 10:57:05 +0000

    modificar
    preparando para modificar

--- compiler/7b2fb3a7b12c3c0751283914725ed2a26171df35_0.file.editar.tpl.php
+++ compiler/7b2fb3a7b12c3c0751283914725ed2a26171df35_0.file.editar.tpl.php
@@ -1,7 +1,7 @@
-<?php /* Smarty version 3.1.27, created on 2016-05-09 20:15:07
+<?php /* Smarty version 3.1.27, created on 2016-05-10 10:41:33
          compiled from "/home/ubuntu/workspace/styles/templates/acciones/editar.tpl" */ ?>
 <?php
-/*%%SmartyHeaderCode:9219969045730efcb2bbfc0_04084491%%*/
+/*%%SmartyHeaderCode:13570445395731badd6ae4a6_44401024%%*/
 if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
@@ -9,25 +9,35 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '7b2fb3a7b12c3c0751283914725ed2a26171df35' => 
     array (
       0 => '/home/ubuntu/workspace/styles/templates/acciones/editar.tpl',
-      1 => 1462824904,
+      1 => 1462876891,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '9219969045730efcb2bbfc0_04084491',
+  'nocache_hash' => '13570445395731badd6ae4a6_44401024',
   'variables' => 
   array (
+    'id' => 0,
     'nombre' => 0,
+    'reino' => 0,
+    'subreino' => 0,
+    'filo' => 0,
+    'subfilo' => 0,
+    'clase' => 0,
+    'orden' => 0,
+    'familia' => 0,
+    'genero' => 0,
+    'especie' => 0,
     'imagen' => 0,
   ),
   'has_nocache_code' => false,
   'version' => '3.1.27',
-  'unifunc' => 'content_5730efcb2d93f8_48968618',
+  'unifunc' => 'content_5731badd7115e1_39401689',
 ),false);
 /*/%%SmartyHeaderCode%%*/
-if ($_valid && !is_callable('content_5730efcb2d93f8_48968618')) {
-function content_5730efcb2d93f8_48968618 ($_smarty_tpl) {
+if ($_valid && !is_callable('content_5731badd7115e1_39401689')) {
+function content_5731badd7115e1_39401689 ($_smarty_tpl) {
 
-$_smarty_tpl->properties['nocache_hash'] = '9219969045730efcb2bbfc0_04084491';
+$_smarty_tpl->properties['nocache_hash'] = '13570445395731badd6ae4a6_44401024';
 echo $_smarty_tpl->getSubTemplate ('overall/header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0);
 ?>
 
@@ -39,7 +49,8 @@ echo $_smarty_tpl->getSubTemplate ('overall/header.tpl', $_smarty_tpl->cache_id,
 
     <div class="container" style="margin-top: 30px;">
         <center>
-            <form action="?view=nuevo" method="post" enctype="multipart/form-data" class="form-horizontal">
+            <form action="?view=editar&id=<?php echo $_smarty_tpl->tpl_vars['id']->value;?>
+" method="post" enctype="multipart/form-data" class="form-horizontal">
             <label for="nombre" class="col-sm-2 control-label">Nombre</label>
             <div class="col-sm-10">
             <input type="text" name="nombre" id="nombre" class="form-control" placeholder="Introduzca el nombre del animal" required="" autofocus="" value="<?php echo $_smarty_tpl->tpl_vars['nombre']->value;?>
@@ -47,45 +58,56 @@ echo $_smarty_tpl->getSubTemplate ('overall/header.tpl', $_smarty_tpl->cache_id,
             </div>
             <label for="reino" class="col-sm-2 control-label">Reino</label>
             <div class="col-sm-10">
-            <input type="text" name="reino" id="reino" class="form-control" placeholder="Introduzca el reino" required="" autofocus=""><br>
+            <input type="text" name="reino" id="reino" class="form-control" placeholder="Introduzca el reino" required="" autofocus="" value="<?php echo $_smarty_tpl->tpl_vars['reino']->value;?>
+"><br>
             </div>
             <label for="subreino" class="col-sm-2 control-label">Subreino</label>
             <div class="col-sm-10">
-            <input type="text" name="subreino" id="subreino" class="form-control" placeholder="Introduzca el subreino" required="" autofocus=""><br>
+            <input type="text" name="subreino" id="subreino" class="form-control" placeholder="Introduzca el subreino" required="" autofocus="" value="<?php echo $_smarty_tpl->tpl_vars['subreino']->value;?>
+"><br>
             </div>
             <label for="filo" class="col-sm-2 control-label">Filo</label>
             <div class="col-sm-10">
-            <input type="text" name="filo" id="filo" class="form-control" placeholder="Introduzca el filo" required="" autofocus=""><br>
+            <input type="text" name="filo" id="filo" class="form-control" placeholder="Introduzca el filo" required="" autofocus="" value="<?php echo $_smarty_tpl->tpl_vars['filo']->value;?>
+"><br>
             </div>
             <label for="subfilo" class="col-sm-2 control-label">Subfilo</label>
             <div class="col-sm-10">
-            <input type="text" name="subfilo" id="subfilo" class="form-control" placeholder="Introduzca el subfilo" required="" autofocus=""><br>
+            <input type="text" name="subfilo" id="subfilo" class="form-control" placeholder="Introduzca el subfilo" required="" autofocus="" value="<?php echo $_smarty_tpl->tpl_vars['subfilo']->value;?>
+"><br>
             </div>
             <label for="clase" class="col-sm-2 control-label">Clase</label>
             <div class="col-sm-10">
-            <input type="text" name="clase" id="clase" class="form-control" placeholder="Introduzca el clase" required="" autofocus=""><br>
+            <input type="text" name="clase" id="clase" class="form-control" placeholder="Introduzca el clase" required="" autofocus="" value="<?php echo $_smarty_tpl->tpl_vars['clase']->value;?>
+"><br>
             </div>
             <label for="orden" class="col-sm-2 control-label">Orden</label>
             <div class="col-sm-10">
-            <input type="text" name="orden" id="orden" class="form-control" placeholder="Introduzca el orden" required="" autofocus=""><br>
+            <input type="text" name="orden" id="orden" class="form-control" placeholder="Introduzca el orden" required="" autofocus="" value="<?php echo $_smarty_tpl->tpl_vars['orden']->value;?>
+"><br>
             </div>
             <label for="familia" class="col-sm-2 control-label">Familia</label>
             <div class="col-sm-10">
-            <input type="text" name="familia" id="familia" class="form-control" placeholder="Introduzca la familia" required="" autofocus=""><br>
+            <input type="text" name="familia" id="familia" class="form-control" placeholder="Introduzca la familia" required="" autofocus="" value="<?php echo $_smarty_tpl->tpl_vars['familia']->value;?>
+"><br>
             </div>
             <label for="genero" class="col-sm-2 control-label">Genero</label>
             <div class="col-sm-10">
-            <input type="text" name="genero" id="genero" class="form-control" placeholder="Introduzca el genero" required="" autofocus=""><br>
+            <input type="text" name="genero" id="genero" class="form-control" placeholder="Introduzca el genero" required="" autofocus="" value="<?php echo $_smarty_tpl->tpl_vars['genero']->value;?>
+"><br>
             </div>
             <label for="especie" class="col-sm-2 control-label">Especie</label>
             <div class="col-sm-10">
-            <input type="text" name="especie" id="especie" class="form-control" placeholder="Introduzca la especie" required="" autofocus=""><br>
+            <input type="text" name="especie" id="especie" class="form-control" placeholder="Introduzca la especie" required="" autofocus="" value="<?php echo $_smarty_tpl->tpl_vars['especie']->value;?>
+"><br>
             </div>
             <label for="imagen" class="col-sm-2 control-label">Imagen</label>
             <div class="col-sm-10">
             <img class="img-responsive img-rounded col-md-4" src="<?php echo $_smarty_tpl->tpl_vars['imagen']->value;?>
 "/>
             <input type="file" name="imagen" id="imagen"><br>
+            <input type="hidden" name="id" id="id" value="<?php echo $_smarty_tpl->tpl_vars['id']->value;?>
+"><br>
             </div>
             <label for="imagen" class="col-sm-2 control-label"></label>
               <input class="btn btn-primary btn-block" type="submit" value="Enviar" />
--- styles/templates/acciones/editar.tpl
+++ styles/templates/acciones/editar.tpl
@@ -5,51 +5,52 @@
     {include 'overall/nav.tpl'}
     <div class="container" style="margin-top: 30px;">
         <center>
-            <form action="?view=nuevo" method="post" enctype="multipart/form-data" class="form-horizontal">
+            <form action="?view=editar&id={$id}" method="post" enctype="multipart/form-data" class="form-horizontal">
             <label for="nombre" class="col-sm-2 control-label">Nombre</label>
             <div class="col-sm-10">
             <input type="text" name="nombre" id="nombre" class="form-control" placeholder="Introduzca el nombre del animal" required="" autofocus="" value="{$nombre}"><br>
             </div>
             <label for="reino" class="col-sm-2 control-label">Reino</label>
             <div class="col-sm-10">
-            <input type="text" name="reino" id="reino" class="form-control" placeholder="Introduzca el reino" required="" autofocus=""><br>
+            <input type="text" name="reino" id="reino" class="form-control" placeholder="Introduzca el reino" required="" autofocus="" value="{$reino}"><br>
             </div>
             <label for="subreino" class="col-sm-2 control-label">Subreino</label>
             <div class="col-sm-10">
-            <input type="text" name="subreino" id="subreino" class="form-control" placeholder="Introduzca el subreino" required="" autofocus=""><br>
+            <input type="text" name="subreino" id="subreino" class="form-control" placeholder="Introduzca el subreino" required="" autofocus="" value="{$subreino}"><br>
             </div>
             <label for="filo" class="col-sm-2 control-label">Filo</label>
             <div class="col-sm-10">
-            <input type="text" name="filo" id="filo" class="form-control" placeholder="Introduzca el filo" required="" autofocus=""><br>
+            <input type="text" name="filo" id="filo" class="form-control" placeholder="Introduzca el filo" required="" autofocus="" value="{$filo}"><br>
             </div>
             <label for="subfilo" class="col-sm-2 control-label">Subfilo</label>
             <div class="col-sm-10">
-            <input type="text" name="subfilo" id="subfilo" class="form-control" placeholder="Introduzca el subfilo" required="" autofocus=""><br>
+            <input type="text" name="subfilo" id="subfilo" class="form-control" placeholder="Introduzca el subfilo" required="" autofocus="" value="{$subfilo}"><br>
             </div>
             <label for="clase" class="col-sm-2 control-label">Clase</label>
             <div class="col-sm-10">
-            <input type="text" name="clase" id="clase" class="form-control" placeholder="Introduzca el clase" required="" autofocus=""><br>
+            <input type="text" name="clase" id="clase" class="form-control" placeholder="Introduzca el clase" required="" autofocus="" value="{$clase}"><br>
             </div>
             <label for="orden" class="col-sm-2 control-label">Orden</label>
             <div class="col-sm-10">
-            <input type="text" name="orden" id="orden" class="form-control" placeholder="Introduzca el orden" required="" autofocus=""><br>
+            <input type="text" name="orden" id="orden" class="form-control" placeholder="Introduzca el orden" required="" autofocus="" value="{$orden}"><br>
             </div>
             <label for="familia" class="col-sm-2 control-label">Familia</label>
             <div class="col-sm-10">
-            <input type="text" name="familia" id="familia" class="form-control" placeholder="Introduzca la familia" required="" autofocus=""><br>
+            <input type="text" name="familia" id="familia" class="form-control" placeholder="Introduzca la familia" required="" autofocus="" value="{$familia}"><br>
             </div>
             <label for="genero" class="col-sm-2 control-label">Genero</label>
             <div class="col-sm-10">
-            <input type="text" name="genero" id="genero" class="form-control" placeholder="Introduzca el genero" required="" autofocus=""><br>
+            <input type="text" name="genero" id="genero" class="form-control" placeholder="Introduzca el genero" required="" autofocus="" value="{$genero}"><br>
             </div>
             <label for="especie" class="col-sm-2 control-label">Especie</label>
             <div class="col-sm-10">
-            <input type="text" name="especie" id="especie" class="form-control" placeholder="Introduzca la especie" required="" autofocus=""><br>
+            <input type="text" name="especie" id="especie" class="form-control" placeholder="Introduzca la especie" required="" autofocus="" value="{$especie}"><br>
             </div>
             <label for="imagen" class="col-sm-2 control-label">Imagen</label>
             <div class="col-sm-10">
             <img class="img-responsive img-rounded col-md-4" src="{$imagen}"/>
             <input type="file" name="imagen" id="imagen"><br>
+            <input type="hidden" name="id" id="id" value="{$id}"><br>
             </div>
             <label for="imagen" class="col-sm-2 control-label"></label>
               <input class="btn btn-primary btn-block" type="submit" value="Enviar" />
